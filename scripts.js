35ba1cd89380637e426563b048c7f5b7e24897c6
Mariana Vanucci <marianavanucciandrade@gmail.com>
2021-09-23 23:13:37 -0300

    inicial commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+console.log('oi');
+console.log('tudo bem?');
+
+// QUESTÃO 1
+
+// Você possui um array A de dados com 1000 elementos, 
+// todos eles números inteiros únicos que servem para 
+// identificar clientes numa plataforma. Faça o que se pede:
+
+// a) Escreva um algoritmo que permita ordenar esses elementos
+
+// b) Escreva um algoritmo que permita encontrar, 
+// dentre esses 1000 elementos do array A,
+// um elemento especificado pelo usuário.
+
+// c) Imagine que você possua outro array B com 1000 elementos também. 
+// Escreva um código para retornar o elemento do array B a partir 
+// do índice do elemento do array A encontrado no item anterior.
