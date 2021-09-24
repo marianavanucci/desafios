92ec972c96e567153b2875c5346bb68317a8c722
Mariana Vanucci <marianavanucciandrade@gmail.com>
2021-09-24 01:03:18 -0300

    question 1 done

--- scripts.js
+++ scripts.js
@@ -9,10 +9,56 @@ console.log('tudo bem?');
 
 // a) Escreva um algoritmo que permita ordenar esses elementos
 
+var numberArray = [40, 1, 5, 200];
+
+function compararNumeros(a, b) {
+  return a - b;
+}
+
+
+console.log('numberArray:', numberArray.join());
+console.log('Ordenada sem função de comparação:', numberArray.sort());
+console.log('Ordenada com compararNumeros:', numberArray.sort(compararNumeros));
+
+
 // b) Escreva um algoritmo que permita encontrar, 
 // dentre esses 1000 elementos do array A,
 // um elemento especificado pelo usuário.
 
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find(element => element == 8);
+
+console.log(found);
+// expected output: 8
+
+
+
+
 // c) Imagine que você possua outro array B com 1000 elementos também. 
 // Escreva um código para retornar o elemento do array B a partir 
 // do índice do elemento do array A encontrado no item anterior.
+
+
+
+
+
+
+
+// // o array a ser ordenado
+// var list = ['Delta', 'alpha', 'CHARLIE', 'bravo'];
+
+// // array temporário que armazena os objetos com o índice e o valor para ordenação
+// var mapped = list.map(function(el, i) {
+//   return { index: i, value: el.toLowerCase() };
+// })
+
+// // ordenando o array mapeado contendo os dados resumidos
+// mapped.sort(function(a, b) {
+//   return +(a.value > b.value) || +(a.value === b.value) - 1;
+// });
+
+// // container para o resultado ordenado
+// var result = mapped.map(function(el){
+//   return list[el.index];
+// });
